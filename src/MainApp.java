25d2569e10df69876c1752459a398cfd86a2595b
GUSTAVO ALMEIDA BORGES ROSA <2102122553@viavarejo.br>
2019-03-16 13:03:27 -0300

    primeiro commmit

---
+++ src/MainApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		dividirValor((new Scanner(System.in)).nextInt());
+		
+		
+	}
+	
+	private static void dividirValor(int valor) {
+	return 10/valor;
+	}
+
+	
+}
